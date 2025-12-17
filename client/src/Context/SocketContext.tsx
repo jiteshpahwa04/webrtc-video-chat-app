38c1e88c9f9e81134bceb7e1acfcf5617c149ba7
Jitesh Pahwa <jiteshofficial2004@gmail.com>
2025-12-18 00:32:23 +0530

    using own local peerjs server

--- client/src/Context/SocketContext.tsx
+++ client/src/Context/SocketContext.tsx
@@ -36,7 +36,11 @@ export const SocketProvider: React.FC<Props> = ({ children }) => {
     useEffect(() => {
 
         const userId = uuidv4();
-        const newPeer = new Peer(userId);
+        const newPeer = new Peer(userId, {
+            host: 'localhost',
+            port: 9000,
+            path: '/myapp'
+        });
         setUser(newPeer);
 
         fetchUserStream();
